4068ec6208456ca3d6d94656930986045b0e0ff2
Vedik Agarwal <91774067+vedik2002@users.noreply.github.com>
2024-08-25 11:59:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -36,7 +36,5 @@ app.get('/bfhl', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+module.exports = app;
